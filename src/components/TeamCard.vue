259ec9462ec97d4f8252f3cca30ba9b2efdb9793
Santiago Rendon Valencia <santiago.rendon@fau.de>
2020-12-05 22:46:04 +0100

    actualizar componente close #3

--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="card h-100">
-    <!-- <img class="card-img-top" src="http://placehold.it/500x500" alt="" /> -->
     <img class="card-img-top" :src="'https://placeimg.com/800/500/'+member.image" alt="" />
-
-    <!-- src="https://placeimg.com/800/350/"+ -->
-    <!-- <img class="card-img-top" :src="member.imagen" alt="alt" /> -->
+        <div>{{member.codigo}}</div>
     <div class="card-body">
       <h4 class="card-title">{{ member.nombre }}</h4>
       <p class="card-text">
         {{member.descripcion}}
       </p>
     </div>
+
     <span>{{ member.rol}}</span>
     <div class="card-footer">
-      <a href="#" class="btn btn-primary">Saber mas!</a>
+      <a href="#" class="btn btn-primary">Contactar {{member.nombre.split(" ",1).join()}}!</a>
     </div>
   </div>
 </template>
